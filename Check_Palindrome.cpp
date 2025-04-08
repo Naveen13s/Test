a38ac9ac857a306f270b52513e2c69cf7e0f6ae0
Naveen <rajnaveen13.av@gmail.com>
2025-04-08 23:45:55 +0530

    Check if a string is a palindrome

---
+++ Check_Palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int start = 0;
+    int end = str.length() - 1;
+
+    while (start < end) {
+        if (str[start] != str[end])
+            return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (isPalindrome(input))
+        cout << input << " is a palindrome." << endl;
+    else
+        cout << input << " is NOT a palindrome." << endl;
+
+    return 0;
+}
