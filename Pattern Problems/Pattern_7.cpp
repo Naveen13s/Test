f4df90dc27a5e56fd0868738711b8688461c2414
Naveen <rajnaveen13.av@gmail.com>
2025-04-14 22:30:44 +0530

    Pattern 7

---
+++ Pattern Problems/Pattern_7.cpp
@@ -0,0 +1,38 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+    *
+   ***
+  *****
+ *******
+*********
+Print the pattern in the function given to you.*/
+
+#include <iostream>
+using namespace std;
+
+// Function to print the centered pyramid pattern
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (int space = 1; space <= n - i; space++) {
+            cout << " ";
+        }
+
+        // Print stars (2*i - 1 stars in each row)
+        for (int star = 1; star <= 2 * i - 1; star++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
