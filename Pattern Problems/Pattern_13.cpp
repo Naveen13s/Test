0485a53aa25d7be5019c8db9d7fa2681265434a0
Naveen <rajnaveen13.av@gmail.com>
2025-04-16 20:53:42 +0530

    Pattern 13

--- Pattern Problems/Pattern_13.cpp
+++ Pattern Problems/Pattern_13.cpp
@@ -1,7 +1,4 @@
 /*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
-
-
-
 1 
 
 2 3 
@@ -12,7 +9,29 @@
 
 11 12 13 14 15
 
-
-
 Print the pattern in the function given to you. */
 
+#include <iostream>
+using namespace std;
+
+// Function to print the number triangle pattern
+void printPattern(int n) {
+    int num = 1; // Start from 1
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
