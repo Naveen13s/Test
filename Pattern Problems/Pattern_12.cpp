37f044fdd20ca6935ceb768a471f55f22e81b3a8
Naveen <rajnaveen13.av@gmail.com>
2025-04-16 20:50:29 +0530

    Pattern 12

--- Pattern Problems/Pattern_12.cpp
+++ Pattern Problems/Pattern_12.cpp
@@ -8,3 +8,39 @@
 
 
 Print the pattern in the function given to you.  */
+
+#include <iostream>
+using namespace std;
+
+// Function to print the mirrored number pattern
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        // Print increasing numbers from 1 to i
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+
+        // Print spaces: 2 * (n - i)
+        int spaces = 2 * (n - i);
+        for (int s = 1; s <= spaces; s++) {
+            cout << " ";
+        }
+
+        // Print decreasing numbers from i to 1
+        for (int j = i; j >= 1; j--) {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
