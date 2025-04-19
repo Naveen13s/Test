be7e959b74d78d4084db54ee19e33bb13226624e
Naveen <rajnaveen13.av@gmail.com>
2025-04-19 21:28:25 +0530

    Patter_20

--- Pattern Problems/Pattern_20.cpp
+++ Pattern Problems/Pattern_20.cpp
@@ -15,3 +15,45 @@
 
 Print the pattern in the function given to you.  */
 
+#include <iostream>
+using namespace std;
+
+void printPattern(int n) {
+    // Top half
+    for (int i = 1; i <= n; i++) {
+        // Left stars
+        for (int j = 1; j <= i; j++) cout << "*";
+
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) cout << " ";
+
+        // Right stars
+        for (int j = 1; j <= i; j++) cout << "*";
+
+        cout << endl;
+    }
+
+    // Bottom half
+    for (int i = n - 1; i >= 1; i--) {
+        // Left stars
+        for (int j = 1; j <= i; j++) cout << "*";
+
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) cout << " ";
+
+        // Right stars
+        for (int j = 1; j <= i; j++) cout << "*";
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
