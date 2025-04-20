57dc63b49eba96a2bc8419fe163dcd86bb59ba21
Naveen <rajnaveen13.av@gmail.com>
2025-04-20 22:43:35 +0530

    Pattern_21

--- Pattern Problems/Pattern_21.cpp
+++ Pattern Problems/Pattern_21.cpp
@@ -12,3 +12,34 @@
 Print the pattern in the function given to you.  */
 
 
+#include <iostream>
+using namespace std;
+
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        // First and last rows: all stars
+        if (i == 1 || i == n) {
+            for (int j = 1; j <= n; j++) {
+                cout << "*";
+            }
+        } else {
+            // Middle rows: star, spaces, star
+            cout << "*";
+            for (int j = 1; j <= n - 2; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
