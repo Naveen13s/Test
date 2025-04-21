4e723d1ebd0bc62af6768b184635e655f4b8176d
Naveen <rajnaveen13.av@gmail.com>
2025-04-21 23:04:46 +0530

    Count number of odd digits in a number

---
+++ Basic Math/Count_oddno.cpp
@@ -0,0 +1,15 @@
+// Count number of odd digits in a number.
+/*You are given an integer n. You need to return the number of odd digits present in the number.
+The number will have no leading zeroes, except when the number is 0 itself.
+
+Examples:
+Input: n = 5
+Output: 1
+Explanation: 5 is an odd digit.
+
+Input: n = 25
+Output: 1
+Explanation: The only odd digit in 25 is 5.   */
+
+
+
