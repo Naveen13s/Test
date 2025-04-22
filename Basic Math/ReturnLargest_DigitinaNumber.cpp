02a0d450120e3f3f7e542ed9a4796b3b8c6d8eb3
Naveen <rajnaveen13.av@gmail.com>
2025-04-22 22:05:16 +0530

    Return the Largest Digit in a Number

---
+++ Basic Math/ReturnLargest_DigitinaNumber.cpp
@@ -0,0 +1,18 @@
+//Return the Largest Digit in a Number
+/*You are given an integer n. Return the largest digit present in the number.
+
+Examples:
+Input: n = 25
+Output: 5
+Explanation: The largest digit in 25 is 5.
+
+Input: n = 99
+Output: 9
+Explanation: The largest digit in 99 is 9.
+
+Input: n = 1
+Output: 3
+Constraints:
+0 <= n <= 5000
+n will contain no leading zeroes except when it is 0 itself.   */
+
