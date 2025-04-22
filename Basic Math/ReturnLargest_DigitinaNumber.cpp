65ce53f0f7139d73c13c92cb6f5c1a1bbbb47c2f
Naveen <rajnaveen13.av@gmail.com>
2025-04-22 22:05:45 +0530

    Return the Largest Digit in a Number

--- Basic Math/ReturnLargest_DigitinaNumber.cpp
+++ Basic Math/ReturnLargest_DigitinaNumber.cpp
@@ -16,3 +16,22 @@ Constraints:
 0 <= n <= 5000
 n will contain no leading zeroes except when it is 0 itself.   */
 
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int largestDigit(int n) {
+        n = abs(n); // Take absolute value
+        if (n == 0) return 0; // Return for zero case
+        int ans = INT_MIN;
+        while (n != 0) {
+            int d = n % 10;
+            if (d > ans) {
+                ans = d;
+            }
+            n = n / 10;
+        }
+        return ans;
+    }
+};
+
