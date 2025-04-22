bce314527b1bbe4c2d2ad1d1d04692398612fba1
Naveen <rajnaveen13.av@gmail.com>
2025-04-22 21:55:52 +0530

    Palindrome Number

---
+++ Basic Math/Palindrome_Number.cpp
@@ -0,0 +1,18 @@
+//Palindrome Number
+
+/*You are given an integer n. You need to check whether the number is a palindrome number or not. Return true if it's a palindrome number, otherwise return false.
+A palindrome number is a number which reads the same both left to right and right to left.
+
+Examples:
+Input: n = 121
+output: true
+Explanation: When read from left to right : 121.
+When read from right to left : 121.
+
+Input: n = 123
+Output: false
+Explanation: When read from left to right : 123.
+When read from right to left : 321.
+
+Input: 101
+Output:*/
