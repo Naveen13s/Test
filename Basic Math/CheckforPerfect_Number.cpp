919578a400b5e030859dac1688f94d9549bc58f4
Naveen <rajnaveen13.av@gmail.com>
2025-04-23 21:00:34 +0530

    Check for Perfect Number

---
+++ Basic Math/CheckforPerfect_Number.cpp
@@ -0,0 +1,15 @@
+//Check for Perfect Number
+/*You are given an integer n. You need to check if the number is a perfect number or not. Return true if it is a perfect number, otherwise, return false.
+A perfect number is a number whose proper divisors add up to the number itself.
+
+Examples:
+Input: n = 6
+Output: true
+Explanation: Proper divisors of 6 are 1, 2, 3.
+1 + 2 + 3 = 6.
+
+Input: n = 4
+Output: false
+Explanation: Proper divisors of 4 are 1, 2.
+1 + 2 = 3.  */
+
