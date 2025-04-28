80d143021ba2f343fd84aef276f60a8c57c588ea
Naveen <rajnaveen13.av@gmail.com>
2025-04-28 22:03:46 +0530

    Reverse a String II

--- Basic Strings/Reversea_StringII.cpp
+++ Basic Strings/Reversea_StringII.cpp
@@ -15,3 +15,21 @@ Input : s = ["h", "a" ,"n" ,"n" ,"a", "h"]
 Output: ["h", "a" ,"n" ,"n" ,"a", "h"]
 Constraints: 1 <= s.length <= 105
 s consist of only lowercase and uppercase English characters.  */
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+    std::vector<char> reverseString(std::vector<char>& s) {
+        int i = 0; // Initialize pointer i to the start of the string
+        int j = s.size() - 1; // Initialize pointer j to the end of the string
+
+        while (i < j) {
+            // Swap the characters at i and j
+            std::swap(s[i], s[j]);
+            i++; // Move i towards the center
+            j--; // Move j towards the center
+        }
+        return s;
+    }
+};
