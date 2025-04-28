0c601fc7a60b2c1f506128f7d863dd7d5da3a2f6
Naveen <rajnaveen13.av@gmail.com>
2025-04-28 21:46:02 +0530

    Sum of Highest and Lowest Frequency

---
+++ Basic Hashing/Sum_Problem.cpp
@@ -0,0 +1,16 @@
+//Sum of Highest and Lowest Frequency
+/*Given an array of n integers, find the sum of the frequencies of the highest occurring number and lowest occurring number.
+Examples:
+Input: arr = [1, 2, 2, 3, 3, 3]
+Output: 4
+Explanation: The highest frequency is 3 (element 3), and the lowest frequency is 1 (element 1). Their sum is 3 + 1 = 4.
+
+Input: arr = [4, 4, 5, 5, 6]
+Output: 3
+Explanation: The highest frequency is 2 (elements 4 and 5), and the lowest frequency is 1 (element 6). Their sum is 2 + 1 = 3.
+
+Input: arr = [10, 9, 7, 7, 8, 8, 8]
+Output: 4
+Constraints: 1 <= n <= 105
+1 <= arr[i] <= 104
+*/
