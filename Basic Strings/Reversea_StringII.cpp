93560b3a13fa65f5cf406869d49c508b0c8b7e09
Naveen <rajnaveen13.av@gmail.com>
2025-04-28 21:59:22 +0530

    Reverse a String II

---
+++ Basic Strings/Reversea_StringII.cpp
@@ -0,0 +1,17 @@
+//Reverse a String II
+/*Given a string, the task is to reverse it. The string is represented by an array of characters s. Perform the reversal in place with O(1) extra memory.
+Note: no need to return anything, modify the given list.
+
+Examples:
+Input : s = ["h", "e" ,"l" ,"l" ,"o"]
+Output : ["o", "l", "l", "e", "h"]
+Explanation : The given string is s = "hello" and after reversing it becomes s = "olleh".
+
+Input : s = ["b", "y" ,"e" ]
+Output : ["e", "y", "b"]
+Explanation : The given string is s = "bye" and after reversing it becomes s = "eyb".
+
+Input : s = ["h", "a" ,"n" ,"n" ,"a", "h"]
+Output: ["h", "a" ,"n" ,"n" ,"a", "h"]
+Constraints: 1 <= s.length <= 105
+s consist of only lowercase and uppercase English characters.  */
