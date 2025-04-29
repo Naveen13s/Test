d08757dcf47517f026bcef40faf3bb8f14acfc69
Naveen <rajnaveen13.av@gmail.com>
2025-04-29 20:59:54 +0530

    Palindrome Check

---
+++ Basic Strings/Palindrome_Check.cpp
@@ -0,0 +1,18 @@
+//Palindrome Check
+/*You are given a string s. Return true if the string is palindrome, otherwise false. A string is called palindrome if it reads the same forward and backward.
+Examples:
+Input : s = "hannah"
+Output : true
+Explanation : The given string when read backward is -> "hannah", which is same as when read forward.
+Hence answer is true.
+
+Input : s = "aabbaaa"
+Output : false
+Explanation : The given string when read backward is -> "aaabbaa", which is not same as when read forward.
+Hence answer is false.
+
+Input : s = "aabbccbbaa"
+Output: true
+Constraints:
+1 <= s.length <= 105
+s consist of only uppercase and lowercase English characters.  */
