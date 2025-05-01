6fa2f27ea8acb2cce16cf0d2e6e620a14127a482
Naveen <rajnaveen13.av@gmail.com>
2025-05-02 00:06:33 +0530

    Valid Anagram

--- Basic Strings/Valid_Anagram.cpp
+++ Basic Strings/Valid_Anagram.cpp
@@ -11,4 +11,15 @@ Input : s = "dog" , t = "cat"
 Output : false
 Explanation : We cannot rearrange the characters of string s to get string t as frequency of all characters from both strings is not same.*/
 
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool anagramStrings(string &s, string t) {
+        if (s.size() != t.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
 
