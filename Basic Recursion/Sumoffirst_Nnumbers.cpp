5d96699cd6e8c54189180f72cdcbc7dbf3d58539
Naveen <rajnaveen13.av@gmail.com>
2025-05-04 20:36:40 +0530

    Sum of First N Numbers

---
+++ Basic Recursion/Sumoffirst_Nnumbers.cpp
@@ -0,0 +1,18 @@
+//Sum of First N Numbers
+/*Given an integer N, return the sum of first N natural numbers. Try to solve this using recursion.
+Examples:
+Input : N = 4
+Output : 10
+Explanation : first four natural numbers are 1, 2, 3, 4.
+Sum is 1 + 2 + 3 + 4 => 10.
+
+Input : N = 2
+Output : 3
+Explanation : first two natural numbers are 1, 2.
+Sum is 1 + 2 => 3.
+
+Input : N = 10
+Output: 55
+Constraints:
+1 <= N <= 103
+*/
