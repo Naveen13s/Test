b83e90d39c589db8781967fb494cac2b4c989e16
Naveen <rajnaveen13.av@gmail.com>
2025-05-05 20:53:57 +0530

    Basic Problem on Resursion

---
+++ Basic Recursion/ReverseaStringI.cpp
@@ -0,0 +1,16 @@
+//Reverse a String I
+/*Given an input string as an array of characters, write a function that reverses the string.
+
+Examples:
+Input : s = ["h", "e", "l", "l", "o"]
+Output : ["o", "l", "l", "e", "h"]
+Explanation : The given string is s = "hello" and after reversing it becomes s = "olleh".
+
+Input : s = ["b", "y", "e" ]
+Output : ["e", "y", "b"]
+Explanation : The given string is s = "bye" and after reversing it becomes s = "eyb".
+
+Input : s = ["h", "a", "n", "n", "a", "h"]
+Output: ["h", "a", "n", "n", "a", "h"]
+Constraints: 1 <= s.length <= 103
+s consist of only lowercase and uppercase English characters.  */
