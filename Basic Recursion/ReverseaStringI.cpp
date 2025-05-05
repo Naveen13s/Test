aff3d6a7b6b0ffb0b183f782bd3168db716dec87
Naveen <rajnaveen13.av@gmail.com>
2025-05-05 21:04:33 +0530

    Reverse a String I

--- Basic Recursion/ReverseaStringI.cpp
+++ Basic Recursion/ReverseaStringI.cpp
@@ -14,3 +14,21 @@ Input : s = ["h", "a", "n", "n", "a", "h"]
 Output: ["h", "a", "n", "n", "a", "h"]
 Constraints: 1 <= s.length <= 103
 s consist of only lowercase and uppercase English characters.  */
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void reverse(int l, int r, vector<char>& s) {
+        if (l >= r) return;
+        swap(s[l], s[r]);
+        reverse(l + 1, r - 1, s);
+    }
+
+    vector<char> reverseString(vector<char>& s) {
+        int l = 0;
+        int r = s.size() - 1;
+        reverse(l, r, s);
+        return s;
+    }
+};
