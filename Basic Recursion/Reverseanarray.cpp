fa7e440b4eac96389b7337259459c040af0f238a
Naveen <rajnaveen13.av@gmail.com>
2025-05-09 21:50:25 +0530

    Reverse an array

---
+++ Basic Recursion/Reverseanarray.cpp
@@ -0,0 +1,14 @@
+//Reverse an array
+/*Given an array nums of n integers, return reverse of the array.
+
+Examples:
+Input : nums = [1, 2, 3, 4, 5]
+Output : [5, 4, 3, 2, 1]
+
+Input : nums = [1, 3, 3, 3, 5]
+Output : [5, 3, 3, 3, 1]
+
+Input : nums = [1, 2, 1]
+Output: [1, 2, 1]
+Constraints: 1 <= n <= 100
+1 <= nums[i] <= 100  */
