0f14423480155e99c377ecafdd1f1351d15816c7
Naveen <rajnaveen13.av@gmail.com>
2025-05-09 21:58:13 +0530

    Check if the Array is Sorted II

--- Basic Recursion/CheckiftheArrayis_SortedII.cpp
+++ Basic Recursion/CheckiftheArrayis_SortedII.cpp
@@ -1,2 +1,29 @@
 //Check if the Array is Sorted II
 /**/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        bool isSorted(vector<int>& nums) {
+            // Base case: if the vector has one or no element, it's sorted
+            if (nums.size() <= 1) {
+                return true;
+            }
+            return sort(nums, 0, 1);
+        }
+    private:
+        bool sort(vector<int>& nums, int first, int second) {
+            // Check if we've reached the end of the vector
+            if (second == nums.size()) {
+                return true;
+            }
+            // Return false if the current elements are out of order
+            if (nums[first] > nums[second]) {
+                return false;
+            }
+            // Recurse to the next pair
+            return sort(nums, first + 1, second + 1);
+        }
+};
