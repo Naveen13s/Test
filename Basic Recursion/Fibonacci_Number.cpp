d040e7fdaf3529c8f6eb31f79939e024cb2207c5
Naveen <rajnaveen13.av@gmail.com>
2025-05-10 23:45:08 +0530

    Fibonacci Number

--- Basic Recursion/Fibonacci_Number.cpp
+++ Basic Recursion/Fibonacci_Number.cpp
@@ -13,3 +13,14 @@ Explanation : F(2) = F(1) + F(0) => 1 + 0 => 1.
 Input : n = 3
 Output : 2
 Explanation : F(3) = F(2) + F(1) => 1 + 1 => 2.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1) + fib(n-2);
+    }
+};
