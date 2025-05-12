b567bbf3b37067b5a9283177b14117cbfbe17c69
Naveen <rajnaveen13.av@gmail.com>
2025-05-12 22:36:06 +0530

    Added new solution

--- Pattern Problems/Pattern_5.cpp
+++ Pattern Problems/Pattern_5.cpp
@@ -8,23 +8,16 @@ Print the pattern in the function given to you.*/
 
 #include <iostream>
 using namespace std;
-
-// Function to print the inverted star pattern
-void printPattern(int n) {
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
+class Solution {
+    public:
+        void pattern5(int n) {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 1; j <= (n - i); j++)
+                {
+                    std::cout << "*";
+                }
+                std::cout << std::endl;
+            }
         }
-        cout << endl;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-
-    printPattern(n);
-
-    return 0;
-}
+    };
