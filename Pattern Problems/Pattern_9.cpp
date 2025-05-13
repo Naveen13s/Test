100e7394c26f8f308b1789b659db400f43358c06
Naveen <rajnaveen13.av@gmail.com>
2025-05-13 22:22:13 +0530

    Added new solution with comment

--- Pattern Problems/Pattern_9.cpp
+++ Pattern Problems/Pattern_9.cpp
@@ -14,42 +14,32 @@ Print the pattern in the function given to you.*/
 
 #include <iostream>
 using namespace std;
-
-// Function to print full diamond pattern
-void printPattern(int n) {
-    // Upper pyramid (including middle line)
-    for (int i = 1; i <= n; i++) {
-        // Spaces
-        for (int space = 1; space <= n - i; space++) {
-            cout << " ";
+class Solution {
+    public:
+        void pattern9(int n) {
+            // Upper part of the pattern
+            for (int i = 0; i < n; i++) {
+                // Print leading spaces
+                for (int j = 0; j < (n - i - 1); j++) {
+                    cout << " ";
+                }
+                // Print stars
+                for (int j = 0; j < (2 * i + 1); j++) {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+            // Lower part of the pattern
+            for (int i = n - 1; i >= 0; i--) {
+                // Print leading spaces
+                for (int j = 0; j < (n - i - 1); j++) {
+                    cout << " ";
+                }
+                // Print stars
+                for (int j = 0; j < (2 * i + 1); j++) {
+                    cout << "*";
+                }
+                cout << endl;
+            }
         }
-        // Stars
-        for (int star = 1; star <= 2 * i - 1; star++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    // Lower inverted pyramid
-    for (int i = n; i >= 1; i--) {
-        // Spaces
-        for (int space = 1; space <= n - i; space++) {
-            cout << " ";
-        }
-        // Stars
-        for (int star = 1; star <= 2 * i - 1; star++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-
-    printPattern(n);
-
-    return 0;
-}
+    };
