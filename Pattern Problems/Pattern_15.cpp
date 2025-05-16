a4b28c5c0cb5e8ca11e01e43e977a20a45da4141
Naveen <rajnaveen13.av@gmail.com>
2025-05-16 22:35:26 +0530

    Solution Updated

--- Pattern Problems/Pattern_15.cpp
+++ Pattern Problems/Pattern_15.cpp
@@ -8,22 +8,14 @@ Print the pattern in the function given to you.  */
 
 #include <iostream>
 using namespace std;
-
-void printPattern(int n) {
-    for (int i = n; i >= 1; i--) {
-        for (char ch = 'A'; ch < 'A' + i; ch++) {
-            cout << ch;
+class Solution {
+    public:
+        void pattern15(int n) {
+            for (int i = 0; i < n; i++) {
+                for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++) {
+                    cout << ch;
+                }
+                cout << endl;
+            }
         }
-        cout << endl;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-
-    printPattern(n);
-
-    return 0;
-}
+    };
