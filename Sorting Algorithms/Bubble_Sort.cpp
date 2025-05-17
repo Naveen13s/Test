93f7268f02a810a0835cde1849fe829aecdd28f4
Naveen <rajnaveen13.av@gmail.com>
2025-05-17 23:15:06 +0530

    Bubble Sort

--- Sorting Algorithms/Bubble_Sort.cpp
+++ Sorting Algorithms/Bubble_Sort.cpp
@@ -1,2 +1,39 @@
 //Bubble Sort
-/**/
+/*Given an array of integers called nums,sort the array in non-decreasing order using the bubble sort algorithm and return the sorted array.
+A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
+
+Examples:
+Input: nums = [7, 4, 1, 5, 3]
+Output: [1, 3, 4, 5, 7]
+Explanation: 1 <= 3 <= 4 <= 5 <= 7.
+Thus the array is sorted in non-decreasing order.
+
+Input: nums = [5, 4, 4, 1, 1]
+Output: [1, 1, 4, 4, 5]Explanation: 1 <= 1 <= 4 <= 4 <= 5.
+Thus the array is sorted in non-decreasing order.
+
+Input: nums = [3, 2, 3, 4, 5]
+Output: [2, 3, 3, 4, 5]
+Constraints:
+1 <= nums.length <= 1000
+-104 <= nums[i] <= 104
+nums[i] may contain duplicate values. */
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        vector<int> bubbleSort(vector<int>& nums) {
+            int n = nums.size();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (nums[j] > nums[j + 1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    };
