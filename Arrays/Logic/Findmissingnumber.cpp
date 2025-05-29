d2794bab3c52078574eb6eb8dce878970347d35b
Naveen <rajnaveen13.av@gmail.com>
2025-05-29 20:43:09 +0530

    Find missing number

--- Arrays/Logic/Findmissingnumber.cpp
+++ Arrays/Logic/Findmissingnumber.cpp
@@ -17,3 +17,19 @@ n == nums.length
 1 <= n <= 104
 0 <= nums[i] <= n
 All the numbers of nums are unique.   */
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map<int,int> mpp;
+        for(int i=0; i < nums.size(); i++)
+            mpp[nums[i]]++;
+        for(int i=0; i <= nums.size(); i++) {
+            if(mpp[i] == 0)
+                return i;
+        }
+        return -1;
+    }
+};
