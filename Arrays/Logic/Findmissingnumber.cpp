517259352d0d7222d109a8e953d3275b50b1b67b
Naveen <rajnaveen13.av@gmail.com>
2025-05-29 20:41:56 +0530

    Find missing number

---
+++ Arrays/Logic/Findmissingnumber.cpp
@@ -0,0 +1,19 @@
+//Find missing number
+/*Given an integer array of size n containing distinct values in the range from 0 to n (inclusive), return the only number missing from the array within this range.
+
+Examples:
+Input: nums = [0, 2, 3, 1, 4]
+Output: 5
+Explanation: nums contains 0, 1, 2, 3, 4 thus leaving 5 as the only missing number in the range [0, 5]
+
+Input: nums = [0, 1, 2, 4, 5, 6]
+Output: 3
+Explanation: nums contains 0, 1, 2, 4, 5, 6 thus leaving 3 as the only missing number in the range [0, 6]
+
+Input: nums = [1, 3, 6, 4, 2, 5]
+Output:0
+Constraints:
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.   */
