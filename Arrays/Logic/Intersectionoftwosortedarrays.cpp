0651784a4808569f55053964976ec22264c8860d
Naveen <rajnaveen13.av@gmail.com>
2025-06-04 23:06:37 +0530

    Solution

---
+++ Arrays/Logic/Intersectionoftwosortedarrays.cpp
@@ -0,0 +1,18 @@
+//Intersection of two sorted arrays
+/* Given two sorted arrays, nums1 and nums2, return an array containing the intersection of these two arrays. Each element in the result must appear as many times as it appears in both arrays.
+The intersection of two arrays is an array where all values are present in both arrays.
+
+Examples:
+Input: nums1 = [1, 2, 2, 3, 5], nums2 = [1, 2, 7]
+Output: [1, 2]
+Explanation: The elements 1, 2 are the only elements present in both nums1 and nums2
+
+Input: nums1 = [1, 2, 2, 3], nums2 = [4, 5, 7]
+Output: []
+Explanation: No elements appear in both nums1 and nums2
+
+Input: nums1 = [-45, -45, 0, 0, 2], nums2 = [-50, -45, 0, 0, 5, 7]
+Output: [-45, 0, 0]
+Constraints: 1 <= nums1.length, nums2.length <= 1000
+-104 <= nums1[i], nums2[i] <= 104
+Both nums1 and nums2 are sorted in non-decreasing order.  */
