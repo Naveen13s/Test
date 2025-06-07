1f5b57d6841e1192c49bda3455df88e90e268f93
Naveen <rajnaveen13.av@gmail.com>
2025-06-07 20:03:29 +0530

    Solution

--- Arrays/Logic/Intersectionoftwosortedarrays.cpp
+++ Arrays/Logic/Intersectionoftwosortedarrays.cpp
@@ -26,7 +26,6 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         vector<int> ans;
-
         int i = 0, j = 0;
         while (i < n1 && j < n2) {
             if (nums1[i] == nums2[j]) {
