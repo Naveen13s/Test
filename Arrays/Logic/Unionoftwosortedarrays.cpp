08b17c2ecf7d514efc60d1fdce375d8d73e02491
Naveen <rajnaveen13.av@gmail.com>
2025-06-08 23:48:02 +0530

    Solution

--- Arrays/Logic/Unionoftwosortedarrays.cpp
+++ Arrays/Logic/Unionoftwosortedarrays.cpp
@@ -1,7 +1,6 @@
 //Union of two sorted arrays
 /*Given two sorted arrays nums1 and nums2, return an array that contains the union of these two arrays. The elements in the union must be in ascending order.
 The union of two arrays is an array where all values are distinct and are present in either the first array, the second array, or both.
-
 Examples:
 Input: nums1 = [1, 2, 3, 4, 5], nums2 = [1, 2, 7]
 Output: [1, 2, 3, 4, 5, 7]
