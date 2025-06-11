fc31e8227af8658420414f05dea43f00f54192f4
Naveen <rajnaveen13.av@gmail.com>
2025-06-11 22:23:39 +0530

    Search X in sorted array

---
+++ Binary Search/Fundamentals/SearchXinsortedarray.cpp
@@ -0,0 +1,17 @@
+//SearchXinsortedarray.cpp
+/*Given a sorted array of integers nums with 0-based indexing, find the index of a specified target integer. If the target is found in the array, return its index. If the target is not found, return -1.
+Examples:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: The target integer 9 exists in nums and its index is 4
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: The target integer 2 does not exist in nums so return -1
+
+Input: nums = [-1,0,3,5,9,12], target = -1
+Output: 0
+Constraints:
+  1 <= nums.length <= 105
+  -105 < nums[i], target < 105
+  nums is sorted in ascending order.  */
