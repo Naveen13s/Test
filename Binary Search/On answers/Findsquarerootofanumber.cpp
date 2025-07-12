7439166cb35383a70bd8b96545952c56c587a5aa
Naveen <rajnaveen13.av@gmail.com>
2025-07-12 22:39:56 +0530

    Find square root of a number

--- Binary Search/On answers/Findsquarerootofanumber.cpp
+++ Binary Search/On answers/Findsquarerootofanumber.cpp
@@ -1,2 +1,34 @@
 //Find square root of a number
-/**/
+/*Given a positive integer n. Find and return its square root. If n is not a perfect square, then return the floor value of sqrt(n).
+
+Examples:
+Input: n = 36
+Output: 6
+Explanation: 6 is the square root of 36.
+
+Input: n = 28
+Output: 5
+Explanation: The square root of 28 is approximately 5.292. So, the floor value will be 5.
+
+Input: n=50
+Output: 7
+Constraints:
+ 0 <= n <= 231 - 1  */
+
+ #include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long floorSqrt(long long n) {
+        long long ans = 0;
+        for (long long i = 1; i <= n; i++) {
+            long long val = i * i;
+            if (val <= n * 1ll) {
+                ans = i; 
+            } else {
+                break; 
+            }
+        }
+        return ans;
+    }
+};
