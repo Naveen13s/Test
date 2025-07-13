bda6618826238fc2d959fb19686eaf47566aa06d
Naveen <rajnaveen13.av@gmail.com>
2025-07-13 22:04:13 +0530

    Find the smallest divisor

--- Binary Search/On answers/Findthesmallestdivisor.cpp
+++ Binary Search/On answers/Findthesmallestdivisor.cpp
@@ -17,7 +17,37 @@ Output: 8
 Constraints:
 1 <= nums.length <= 5 * 104
  1 <= nums[i] <= 106
- nums.length <= limit <= 106
+ nums.length <= limit <= 106  */
 
+ #include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int computeSum(vector<int>& nums, int divisor) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor; 
+        }
+        return sum;
+    }
 
-*/
+    int smallestDivisor(vector<int>& nums, int limit) {
+        int left = 1;
+        int right = *max_element(nums.begin(), nums.end());
+        int result = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int sum = computeSum(nums, mid);
+
+            if (sum <= limit) {
+                result = mid;       
+                right = mid - 1;
+            } else {
+                left = mid + 1;     
+            }
+        }
+
+        return result;
+    }
+};
