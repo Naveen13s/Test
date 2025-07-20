f6700fad90e5f497ff2c745caf138bf2ba03c8e9
Naveen <rajnaveen13.av@gmail.com>
2025-07-20 21:04:47 +0530

    Insertion at the tail of LL

--- Linked-List/Fundamentals (Single LL)/InsertionatthetailofLL.cpp
+++ Linked-List/Fundamentals (Single LL)/InsertionatthetailofLL.cpp
@@ -1,2 +1,19 @@
 //Insertion at the tail of LL
-/**/
+/*Given the head of a singly linked list and an integer X, insert a node with value X at the tail of the linked list and return the head of the modified list.
+The tail is the last node of the linked list.
+
+Examples:
+Input: head -> 1 -> 2 -> 3, X = 7
+Output: head -> 1 -> 2 -> 3 -> 7
+Explanation: 7 was added as the last node.
+
+Input: head, X = 0
+Output: head -> 0
+Explanation: 0 was added as the last/only node.
+ 
+Input: head -> 5 -> 6, X = 8
+Output: head -> 5 -> 6 -> 8
+Constraints:
+0 <= number of nodes in the Linked List <= 1000
+0 <= ListNode.val <= 100
+0 <= X <= 100    */
