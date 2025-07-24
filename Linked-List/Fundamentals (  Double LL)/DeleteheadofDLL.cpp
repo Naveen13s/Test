949c9ac8efb76d1870142b3444ede7933f99dbfa
Naveen <rajnaveen13.av@gmail.com>
2025-07-24 20:48:52 +0530

    Delete head of DLL

---
+++ Linked-List/Fundamentals (  Double LL)/DeleteheadofDLL.cpp
@@ -0,0 +1,20 @@
+//Delete head of DLL
+/*Given the head of a doubly linked list, remove the node at the head of the linked list and return the head of the modified list.
+The head is the first node of the linked list.
+
+Examples:
+Input: head -> 1 <-> 2 <-> 3
+Output: head -> 2 <-> 3
+Explanation: The node with value 1 was removed.
+
+Input: head -> 7
+Output: headExplanation: Note that the head has null value after the removal.
+
+Input: head -> 2 <-> 4
+
+Output: head -> 4
+Constraints:
+n == Number of nodes in the linked list
+1 <= n <= 100
+0 <= ListNode.val <= 100   */
+
