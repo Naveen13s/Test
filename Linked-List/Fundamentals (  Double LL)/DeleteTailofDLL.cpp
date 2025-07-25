62fafd4f260683c646b365086f8aaf3457e87474
Naveen <rajnaveen13.av@gmail.com>
2025-07-25 20:41:37 +0530

    Delete Tail of DLL

---
+++ Linked-List/Fundamentals (  Double LL)/DeleteTailofDLL.cpp
@@ -0,0 +1,19 @@
+//Delete Tail of DLL
+/*Given the head of a doubly linked list, remove the node at the tail of the linked list and return the head of the modified list.
+The tail is the last node of the linked list.
+
+Examples:
+Input: head -> 1 <-> 2 <-> 3
+Output: head -> 1 <-> 2
+Explanation: The node with value 3 was removed.
+
+Input: head -> 7
+Output: head
+Explanation: Note that the head has null value after the removal.
+
+Input: head -> 2 <-> 4
+Output: head -> 2
+Constraints:
+n == Number of nodes in the linked list
+1 <= n <= 100
+0 <= ListNode.val <= 100    */
