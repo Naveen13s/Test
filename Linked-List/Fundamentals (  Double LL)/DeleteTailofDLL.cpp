ad4a91bdc4394caa9a9ac8d4ed66217f83f5b785
Naveen <rajnaveen13.av@gmail.com>
2025-07-25 20:42:06 +0530

    Delete Tail of DLL

--- Linked-List/Fundamentals (  Double LL)/DeleteTailofDLL.cpp
+++ Linked-List/Fundamentals (  Double LL)/DeleteTailofDLL.cpp
@@ -17,3 +17,51 @@ Constraints:
 n == Number of nodes in the linked list
 1 <= n <= 100
 0 <= ListNode.val <= 100    */
+
+
+/*
+Definition of doubly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode* deleteTail(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;  
+        }
+        ListNode* tail = head;
+        while (tail->next != nullptr) 
+            tail = tail->next;
+        ListNode* newTail = tail->prev;
+        newTail->next = nullptr;
+        tail->prev = nullptr;
+        delete tail;  
+        
+        return head; 
+    }
+};
