4068fc0a47140797dd2fa7bfb2a25c6226e7e4b0
Naveen <rajnaveen13.av@gmail.com>
2025-07-26 20:46:37 +0530

    Delete Kth Element of DLL

--- Linked-List/Fundamentals (  Double LL)/DeleteKthElementofDLL.cpp
+++ Linked-List/Fundamentals (  Double LL)/DeleteKthElementofDLL.cpp
@@ -18,3 +18,71 @@ n == Number of nodes in the linked list
 0 <= ListNode.val <= 100
 1 <= k <= n   */
 
+
+/*
+Definition of doubly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode* deleteKthElement(ListNode* head, int k) {
+        if (head == nullptr) 
+            return nullptr;
+    
+        int count = 0;
+        ListNode* kNode = head;
+        while (kNode != nullptr) {
+            count++;
+            if (count == k) break;
+            kNode = kNode->next;
+        }
+        if (kNode == nullptr) return head; 
+        ListNode* prev = kNode->prev;
+        ListNode* front = kNode->next;
+        if (prev == nullptr && front == nullptr) {
+            delete kNode;
+            return nullptr;
+        }
+        else if (prev == nullptr) {
+            head = front;
+            front->prev = nullptr;
+        }
+        else if (front == nullptr) {
+            prev->next = nullptr;
+        }
+        else {
+            prev->next = front;
+            front->prev = prev;
+        }
+        delete kNode;
+        return head;
+    }
+};
+
+
+
