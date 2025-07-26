8bd230bb196a227692258664b67357fcf6d94cb3
Naveen <rajnaveen13.av@gmail.com>
2025-07-26 20:45:06 +0530

    Delete Kth Element of DLL

---
+++ Linked-List/Fundamentals (  Double LL)/DeleteKthElementofDLL.cpp
@@ -0,0 +1,20 @@
+//Delete Kth Element of DLL
+/*Given the head of a doubly linked list and an integer k, remove the node at the kth position of the linked list and return the head of the modified list.
+
+Examples:
+Input: head -> 2 <-> 5 <-> 7 <-> 9, k = 2
+Output: head -> 2 <-> 7 <-> 9
+Explanation: The node with value 5 was removed.
+
+Input: head -> 2 <-> 5 <-> 7, k = 1
+Output: head -> 5 <-> 7
+Explanation: The node with value 2 was removed, note that the head was modified.
+
+Input: head -> 2 <-> 5 <-> 7, k = 3
+Output: head -> 2 <-> 5
+Constraints:
+n == Number of nodes in the linked list
+1 <= n <= 100
+0 <= ListNode.val <= 100
+1 <= k <= n   */
+
