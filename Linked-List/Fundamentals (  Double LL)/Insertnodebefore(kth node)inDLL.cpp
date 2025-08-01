cb33117b5f4772509fc928e4ca67d8e5b99b36b9
Naveen <rajnaveen13.av@gmail.com>
2025-08-01 20:39:32 +0530

    Insert node before (kth node) in DLL

--- Linked-List/Fundamentals (  Double LL)/Insertnodebefore(kth node)inDLL.cpp
+++ Linked-List/Fundamentals (  Double LL)/Insertnodebefore(kth node)inDLL.cpp
@@ -19,3 +19,55 @@ Constraints:
 •	1 <= K <= n
 
 */
+
+/*
+Definition of doubly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode *prev;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+        prev = NULL;
+    }
+    ListNode(int data1, ListNode *next1, ListNode *prev1)
+    {
+        val = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode* insertBeforeKthPosition(ListNode* head, int X, int K) {
+        if (K == 1) {
+            ListNode* newHead = new ListNode(X, head, nullptr);
+            head->prev = newHead;
+            return newHead;
+        } 
+        ListNode* temp = head;
+        int count = 0;
+        while (temp != nullptr) {
+            count++;
+            if (count == K) break;
+            temp = temp->next;
+        }
+        ListNode* prev = temp->prev;
+        ListNode* newNode = new ListNode(X, temp, prev);
+        prev->next = newNode;
+        temp->prev = newNode;
+        return head;
+    }
+};
