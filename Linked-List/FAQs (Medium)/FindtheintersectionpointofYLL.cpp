701db1a934594712ebc48c076bc82e642004e102
Naveen <rajnaveen13.av@gmail.com>
2025-08-13 00:14:43 +0530

    Find the intersection point of Y LL

--- Linked-List/FAQs (Medium)/FindtheintersectionpointofYLL.cpp
+++ Linked-List/FAQs (Medium)/FindtheintersectionpointofYLL.cpp
@@ -34,3 +34,46 @@ n == number of nodes in listB.
 intersectVal, skipA, skipB is -1 if listA and listB do not intersect.
 intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
 */
+
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> nodes_set;
+        while (headA != NULL) {
+            nodes_set.insert(headA);
+            headA = headA->next;
+        }
+        while (headB != NULL) {
+            if (nodes_set.find(headB) != nodes_set.end()) {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
