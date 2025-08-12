61eee2b2bcecfa9521ad7b5afe0ec09a5fff998f
Naveen <rajnaveen13.av@gmail.com>
2025-08-12 23:12:29 +0530

    Check if LL is palindrome or not

---
+++ Linked-List/FAQs (Medium)/CheckifLLispalindromeornot.cpp
@@ -0,0 +1,20 @@
+//Check if LL is palindrome or not
+/*
+Given the head of a singly linked list representing a positive integer number. Each node of the linked list represents a digit of the number, with the 1st node containing the leftmost digit of the number and so on. Check whether the linked list values form a palindrome or not. Return true if it forms a palindrome, otherwise, return false.
+A palindrome is a sequence that reads the same forward and backwards.
+Examples:
+Input: head -> 3 -> 7 -> 5 -> 7 -> 3
+Output: true
+Explanation: 37573 is a palindrome.
+
+Input: head -> 1 -> 1 -> 2 -> 1
+Output: false
+Explanation: 1121 is not a palindrome.
+
+Input: head -> 9 -> 9 -> 9 -> 9
+Output: true
+Constraints:
+1 <= number of nodes in the Linked List <= 105
+0 <= ListNode.val <= 9
+The number represented does not contain any leading zeroes.
+*/
