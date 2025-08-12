2a49b3d144be79696ed9a2395ab2943bd15467fe
Naveen <rajnaveen13.av@gmail.com>
2025-08-12 23:02:44 +0530

    Reverse a LL

--- Linked-List/Logic Building/ReverseaLL.cpp
+++ Linked-List/Logic Building/ReverseaLL.cpp
@@ -17,3 +17,52 @@ Constraints:
 0 <= number of nodes in the Linked List <= 105
 0 <= ListNode.val <= 104   */
 
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+   public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* current = head;
+
+        ListNode* prev = nullptr;
+
+        while (current != nullptr)
+
+        {
+            ListNode* temp = current->next;
+
+            current->next = prev;
+
+            prev = current;
+
+            current = temp;
+        }
+
+        return prev;
+    }
+};
+
