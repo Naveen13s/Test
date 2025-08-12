2315a24887f4d31e9e485c7522bf19a73c6ad00c
Naveen <rajnaveen13.av@gmail.com>
2025-08-12 23:02:02 +0530

    Reverse a LL

---
+++ Linked-List/Logic Building/ReverseaLL.cpp
@@ -0,0 +1,19 @@
+//Reverse a LL
+/*Given the head of a singly linked list. Reverse the given linked list and return the head of the modified list.
+
+Examples:
+Input: head -> 1 -> 2 -> 3 -> 4 -> 5
+Output: head -> 5 -> 4 -> 3 -> 2 -> 1
+Explanation: All the links are reversed and the head now points to the last node of the original list.
+
+Input: head -> 6 -> 8
+Output: head -> 8 -> 6
+Explanation: All the links are reversed and the head now points to the last node of the original list.
+This can be seen like: 6 <- 8 <- head.
+
+Input: head -> 1
+Output: head -> 1
+Constraints:
+0 <= number of nodes in the Linked List <= 105
+0 <= ListNode.val <= 104   */
+
