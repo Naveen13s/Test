47a79dfaaa7e6ba999c80eca6bf3a7c0d8106a5f
Naveen <rajnaveen13.av@gmail.com>
2025-08-14 22:11:50 +0530

    Pow(x,n)

---
+++ Recursion/mplementation Problems/Pow(x,n).cpp
@@ -0,0 +1,21 @@
+//Pow(x,n)
+/*
+Implement the power function pow(x, n) , which calculates the x raised to n i.e. xn.
+Examples:
+Input : x = 2.0000 , n = 10
+Output : 1024.0000
+Explanation : Answer = 2^10 => 1024.
+Input : x = 2.0000 , n = -2
+Output : 0.2500
+Explanation : Answer = 2^(-2) = 1/4 => 0.25.
+Input : x = 2.5000 , n = 2
+Output:6.25
+Constraints:
+•	-100.0 <= x <= 100.0
+•	-231 <= n <= 231 - 1
+•	-104 <= xn <= 104
+•	Either x is not zero or n>0.
+•	n is an integer.
+
+
+*/
