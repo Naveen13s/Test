32f7ae7ce847a9d130b9d7d656263a98d8817239
Naveen <rajnaveen13.av@gmail.com>
2025-08-14 22:15:24 +0530

    Power Set

---
+++ Recursion/mplementation Problems/GenerateParentheses.cpp
@@ -0,0 +1,15 @@
+//Generate Parentheses
+/*
+Given an integer n.Generate all possible combinations of well-formed parentheses of length 2 x N.
+Examples:
+Input : n = 3
+Output : [ "((()))" , "(()())" , "(())()" , "()(())" , "()()()" ]
+Input : 2
+Output : [ "(())" , "()()" ]
+Input : 1
+Output:()
+Constraints:
+•	1 <= n <= 8
+
+
+*/
---
+++ Recursion/mplementation Problems/PowerSet.cpp
@@ -0,0 +1,17 @@
+//Power Set
+/*
+Given an array of integers nums of unique elements. Return all possible subsets (power set) of the array.
+
+Do not include the duplicates in the answer.
+Examples:
+Input : nums = [1, 2, 3]
+Output : [ [ ] , [1] , [2] , [1, 2] , [3] , [1, 3] , [2, 3] , [1, 2 ,3] ]
+Input : nums = [1, 2]
+Output : [ [ ] , [1] , [2] , [1,2] ]
+Input : nums = [0]
+Output:[ [ ] , [0] ]
+Constraints:
+•	1 <= nums.length <= 10
+•	-10 <= nums[i] <= 10
+
+*/
