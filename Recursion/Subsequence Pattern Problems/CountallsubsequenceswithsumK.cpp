54f7ad0cff51dbcbc3d1af92afee6a2e6e04e76c
Naveen <rajnaveen13.av@gmail.com>
2025-08-16 22:03:20 +0530

    Count all subsequences with sum K

--- Recursion/Subsequence Pattern Problems/CountallsubsequenceswithsumK.cpp
+++ Recursion/Subsequence Pattern Problems/CountallsubsequenceswithsumK.cpp
@@ -17,3 +17,21 @@ Constraints:
 
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+private:
+    int func(int ind, int sum, vector<int> &nums) {
+        if (sum == 0) return 1;
+        if (sum < 0 || ind == nums.size()) return 0;
+        return func(ind + 1, sum - nums[ind], nums) + func(ind + 1, sum, nums);
+    }
+
+public:
+    // Function to start counting subsequences
+    int countSubsequenceWithTargetSum(vector<int>& nums, int target) {
+        return func(0, target, nums);
+    }
+};
+
+
