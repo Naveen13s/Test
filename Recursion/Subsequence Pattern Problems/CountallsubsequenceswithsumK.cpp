a4fa65593836d707a9ef3f18f131c39bd2f0e3f3
Naveen <rajnaveen13.av@gmail.com>
2025-08-16 22:02:51 +0530

    Count all subsequences with sum K

---
+++ Recursion/Subsequence Pattern Problems/CountallsubsequenceswithsumK.cpp
@@ -0,0 +1,19 @@
+//Count all subsequences with sum K
+/*
+Given an array nums and an integer k.Return the number of non-empty subsequences of nums such that the sum of all elements in the subsequence is equal to k.
+Examples:
+Input : nums = [4, 9, 2, 5, 1] , k = 10
+Output : 2
+Explanation : The possible subsets with sum k are [9, 1] , [4, 5, 1].
+Input : nums = [4, 2, 10, 5, 1, 3] , k = 5
+Output : 3
+Explanation : The possible subsets with sum k are [4, 1] , [2, 3] , [5].
+Input : nums = [1, 10, 4, 5] , k = 16
+Output:
+Constraints:
+•	1 <= nums.length <= 20
+•	1 <= nums[i] <= 100
+•	1 <= k <= 2000
+
+*/
+
