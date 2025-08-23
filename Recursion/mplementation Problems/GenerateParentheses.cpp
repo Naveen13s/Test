8d9d04c3d050f0e43763bbcde7bfa93a04d91650
Naveen <rajnaveen13.av@gmail.com>
2025-08-23 13:04:10 +0530

    Generate Parentheses

--- Recursion/mplementation Problems/GenerateParentheses.cpp
+++ Recursion/mplementation Problems/GenerateParentheses.cpp
@@ -12,3 +12,34 @@ Constraints:
 •	1 <= n <= 8
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        generateParenthesisRec(ans, temp, 0, 0, 0, n);
+        return ans;
+    }
+
+    void generateParenthesisRec(vector<string> &ans, string &temp, int idx,int op, int cl, int n) {
+        if (idx == 2 * n and op == n and cl == n) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (op > n) {
+            return;
+        }
+
+        if (cl > op) {
+            return;
+        }
+        generateParenthesisRec(ans, temp += '(', idx + 1, op + 1, cl, n);
+        temp.pop_back();
+        generateParenthesisRec(ans, temp += ')', idx + 1, op, cl + 1, n);
+        temp.pop_back();
+    }
+};
