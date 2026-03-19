2d1dc6530c18a3c870721d49a31bd87a46d7808b
Naveen <rajnaveen13.av@gmail.com>
2026-03-19 23:56:31 +0530

    Added JavaScript Solution

--- Pattern Problems/Pattern_4.cpp
+++ Pattern Problems/Pattern_4.cpp
@@ -19,3 +19,16 @@ class Solution {
             }
         }
     };
+
+    //JavaScript Solution
+    class Solution {
+    pattern4(n) {
+        for (let i=1; i<=n; i++){
+            for (let j=1; j<=i; j++){
+             process.stdout.write(`${i}`);   
+            }
+            console.log();
+        }
+
+    }
+}
