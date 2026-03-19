afa8185986167692ab772babffb5da688dad1c45
Naveen <rajnaveen13.av@gmail.com>
2026-03-19 23:53:32 +0530

    Added JavaScript Solution

--- Pattern Problems/Pattern_1.cpp
+++ Pattern Problems/Pattern_1.cpp
@@ -20,3 +20,16 @@ public:
         }
     }
 };
+
+//Javascript Sol-
+class Solution {
+    pattern1(n) {
+        for (let i = 0; i < n; i++){
+            for (let j=0; j< n; j++){
+            process.stdout.write("*");
+        }
+        console.log();
+        }
+
+    }
+}
