74a97d12a1b779897dbfc36aea5ac11aa5160968
Naveen <rajnaveen13.av@gmail.com>
2026-03-19 23:55:48 +0530

    Added JavaScript Solution

--- Pattern Problems/Pattern_3.cpp
+++ Pattern Problems/Pattern_3.cpp
@@ -20,4 +20,15 @@ public:
     }
 };
 
+//"Added JavaScript Solution"
+class Solution {
+    pattern3(n) {
+        for (let i=1; i<=n; i++ ){
+            for (let j= 1; j<= i; j++){
+                process.stdout.write(`${j}`);
+            }
+            console.log();
+        }
 
+    }
+}
