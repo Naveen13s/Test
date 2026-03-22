7fb30973e356aef2cc655ff254f36a704aca1c2c
Naveen <rajnaveen13.av@gmail.com>
2026-03-22 20:05:28 +0530

    Added JavaScript Solution

--- Pattern Problems/Pattern_7.cpp
+++ Pattern Problems/Pattern_7.cpp
@@ -23,3 +23,21 @@ class Solution {
             }
         }
     };
+
+
+    //Javascript Solution
+
+    class Solution {
+    pattern7(n) {
+        for (let i = 0; i<n; i++){
+            for (let j=0; j<n-i-1; j++){
+                process.stdout.write(" ");
+            }
+            for (let j = 0; j<2*i+1; j++){
+                process.stdout.write("*");
+            }
+            console.log();
+        }
+
+    }
+}
