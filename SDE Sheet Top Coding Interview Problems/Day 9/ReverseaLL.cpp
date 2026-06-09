ac08e0f698600d0e6839a941fe10f35fd02e9dc9
Naveen <rajnaveen13.av@gmail.com>
2026-06-09 14:06:04 +0530

    Reverse a LL

---
+++ SDE Sheet Top Coding Interview Problems/Day 9/ReverseaLL.cpp
@@ -0,0 +1,12 @@
+//Reverse a LL
+/* Given the head of a singly linked list. Reverse the given linked list and return the head of the modified list.
+Example 1
+Input: head -> 1 -> 2 -> 3 -> 4 -> 5
+Output: head -> 5 -> 4 -> 3 -> 2 -> 1
+Explanation: All the links are reversed and the head now points to the last node of the original list.  
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
