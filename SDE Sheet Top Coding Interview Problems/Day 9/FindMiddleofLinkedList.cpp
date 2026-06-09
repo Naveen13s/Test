a593db7accc1633b5563dd6d8ec13a10c1613598
Naveen <rajnaveen13.av@gmail.com>
2026-06-09 14:15:10 +0530

    Find Middle of Linked List

---
+++ SDE Sheet Top Coding Interview Problems/Day 9/FindMiddleofLinkedList.cpp
@@ -0,0 +1,33 @@
+//Find Middle of Linked List
+/* Given the head of a singly Linked List, return the middle node of the Linked List.
+If the Linked List has an even number of nodes, return the second middle one.
+Example 1
+Input: head -> 3 -> 8 -> 7 -> 1 -> 3
+Output(value at returned node): 7
+Explanation: There are 5 nodes, so the middle node is the 3rd Node, with value 7.  */
+
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+
