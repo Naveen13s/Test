3286e4f62cba8724fa0bbcf64cf23f46ee2f1d02
Naveen <rajnaveen13.av@gmail.com>
2026-06-15 20:47:52 +0530

    Fractinal knapsack

---
+++ SDE Sheet Top Coding Interview Problems/Day 15/Fractional_Knapsack.cpp
@@ -0,0 +1,16 @@
+//Fractional Knapsack
+/*  You have n items; the i-th item has value val[i] and weight wt[i].
+    A knapsack can carry at most capacity units of weight.
+    You may take any fraction of an item (i.e. split items).
+    Return the maximum total value that can be placed in the knapsack, rounded to exactly 6 decimal places.
+
+    Example 1
+    Input: val = [60,100,120], wt = [10,20,30], capacity = 50
+    Output: 240.000000
+    Explanation:
+    • Take item 0 (w=10, v=60)
+    • Take item 1 (w=20, v=100)
+    • Take 2⁄3 of item 2 (w=20, v=80)
+    Total value = 60 + 100 + 80 = 240      
+    */
+
