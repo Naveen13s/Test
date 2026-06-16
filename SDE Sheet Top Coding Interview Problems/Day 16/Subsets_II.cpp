d484460bfa2b4d3f4243dd40945059397b122604
Naveen <rajnaveen13.av@gmail.com>
2026-06-16 20:40:39 +0530

    Subsets II

---
+++ SDE Sheet Top Coding Interview Problems/Day 16/Subsets_II.cpp
@@ -0,0 +1,8 @@
+//Subsets II
+/* Given an integer array nums, which can have duplicate entries, provide the power set.
+Duplicate subsets cannot exist in the solution set. Return the answer in any sequence.
+Example 1
+Input : nums = [1, 2, 2]
+Output : [ [ ] , [1] , [1, 2] , [1, 2, 2] , [2] , [2, 2] ] 
+
+*/
