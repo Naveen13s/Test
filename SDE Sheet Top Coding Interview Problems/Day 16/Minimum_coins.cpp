0afa84fc12cbf0d66283cc349aaa0a362e90f49e
Naveen <rajnaveen13.av@gmail.com>
2026-06-16 20:26:52 +0530

    Minimum Coins Sol

---
+++ SDE Sheet Top Coding Interview Problems/Day 16/Minimum_coins.cpp
@@ -0,0 +1,8 @@
+//Minimum coins
+/* Given an integer array of coins representing coins of different denominations and an integer amount representing a total amount of money. Return the fewest number of coins that are needed to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1. There are infinite numbers of coins of each type
+Example 1
+Input: coins = [1, 2, 5], amount = 11
+ Output: 3
+ Explanation: 11 = 5 + 5 + 1. We need 3 coins to make up the amount 11.
+ 
+*/
