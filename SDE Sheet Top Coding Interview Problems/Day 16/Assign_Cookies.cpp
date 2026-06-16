9e756743e0dac1f0f4a510fbec8c677ae1a527aa
Naveen <rajnaveen13.av@gmail.com>
2026-06-16 20:43:47 +0530

    Assign Cookies

--- SDE Sheet Top Coding Interview Problems/Day 16/Assign_Cookies.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 16/Assign_Cookies.cpp
@@ -3,3 +3,17 @@
 Given two arrays, student and cookie, the ith value in the Student array describes the minimum size of cookie that the ith student can be assigned. The jth value in the Cookie array represents the size of the jth cookie. If Cookie[j] >= Student[i], the jth cookie can be assigned to the ith student.
 Maximize the number of students assigned with cookies and output the maximum number.
 */
+class Solution{    
+    public:
+    int findMaximumCookieStudents(vector<int>& student, vector<int>& cookie){
+        sort(student.begin(),student.end());
+        sort(cookie.begin(),cookie.end());
+        int n = student.size(),m = cookie.size();
+        int i=0, j=0;
+        while(i<n and j<m){
+            if(cookie[j] >= student[i])i++;
+            j++;
+        }
+        return i;
+    }
+};
