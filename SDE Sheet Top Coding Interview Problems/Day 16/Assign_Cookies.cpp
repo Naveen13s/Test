8a580e464253937959c33e2b4aa25c4c730bc372
Naveen <rajnaveen13.av@gmail.com>
2026-06-16 20:43:30 +0530

    Assign Cookies

---
+++ SDE Sheet Top Coding Interview Problems/Day 16/Assign_Cookies.cpp
@@ -0,0 +1,5 @@
+//Assign Cookies
+/* Consider a scenario where a teacher wants to distribute cookies to students, with each student receiving at most one cookie.
+Given two arrays, student and cookie, the ith value in the Student array describes the minimum size of cookie that the ith student can be assigned. The jth value in the Cookie array represents the size of the jth cookie. If Cookie[j] >= Student[i], the jth cookie can be assigned to the ith student.
+Maximize the number of students assigned with cookies and output the maximum number.
+*/
