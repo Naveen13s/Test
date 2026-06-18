2943cefd15e9a0e8ae79118e457e224acb513eab
Naveen <rajnaveen13.av@gmail.com>
2026-06-18 17:14:25 +0530

    N Queen

---
+++ SDE Sheet Top Coding Interview Problems/Day 18/N_Queen.cpp
@@ -0,0 +1,10 @@
+//N Queen
+/* The challenge of arranging n queens on a n × n chessboard so that no two queens attack one another is known as the "n-queens puzzle."
+   Return every unique solution to the n-queens puzzle given an integer n. The answer can be returned in any sequence.
+   Every solution has a unique board arrangement for the placement of the n-queens, where 'Q' and '.' stand for a queen and an empty space, respectively.
+   Here are the attack rules for N-Queens:
+   Same Row - No two queens can be in the same row.
+   Same Column - No two queens can be in the same column.
+   Same Diagonal (top-left to bottom-right) - No two queens can share the same diagonal where (row - col) is equal.
+   Same Anti-Diagonal (top-right to bottom-left) - No two queens can share the same anti-diagonal where (row + col) is equal.
+*/
