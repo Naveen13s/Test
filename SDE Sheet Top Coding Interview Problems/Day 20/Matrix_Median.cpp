a3c16b0c566e051f10afb719d8814e9d01aa7d58
Naveen <rajnaveen13.av@gmail.com>
2026-06-20 21:06:48 +0530

    Matrix Median

--- SDE Sheet Top Coding Interview Problems/Day 20/Matrix_Median.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 20/Matrix_Median.cpp
@@ -5,3 +5,40 @@
   Output: 5
   Explanation: If we find the linear sorted array, the array becomes 1 2 3 4 5 6 7 8 9. So, median = 
 */
+
+class Solution {
+public:
+    int findMedian(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            low = min(low, matrix[i][0]);
+            high = max(high, matrix[i][m - 1]);
+        }
+
+        int req = (n * m) / 2;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                count += upper_bound(matrix[i].begin(),
+                                     matrix[i].end(),
+                                     mid) - matrix[i].begin();
+            }
+
+            if (count <= req)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low;
+    }
+};
