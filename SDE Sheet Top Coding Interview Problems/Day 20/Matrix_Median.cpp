f60c5f9b58f1b9444c2ac976d945cd2e40d73c3c
Naveen <rajnaveen13.av@gmail.com>
2026-06-20 21:06:28 +0530

    Matrix Median

---
+++ SDE Sheet Top Coding Interview Problems/Day 20/Matrix_Median.cpp
@@ -0,0 +1,7 @@
+//Matrix Median
+/*Given a 2D array matrix that is row-wise sorted. The task is to find the median of the given matrix.
+  Example 1
+  Input: matrix=[ [1, 4, 9], [2, 5, 6], [3, 7, 8] ] 
+  Output: 5
+  Explanation: If we find the linear sorted array, the array becomes 1 2 3 4 5 6 7 8 9. So, median = 
+*/
