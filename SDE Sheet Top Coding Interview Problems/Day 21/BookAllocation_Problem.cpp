c028589112991e42adbaf528662c42e05b8e575b
Naveen <rajnaveen13.av@gmail.com>
2026-06-21 18:18:35 +0530

    Book Allocation Problem

--- SDE Sheet Top Coding Interview Problems/Day 21/BookAllocation_Problem.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 21/BookAllocation_Problem.cpp
@@ -2,3 +2,53 @@
 /* Given an array nums of n integers, where nums[i] represents the number of pages in the i-th book, and an integer m representing the number of students, allocate all the books to the students so that each student gets at least one book, each book is allocated to only one student, and the allocation is contiguous.
    Allocate the books to m students in such a way that the maximum number of pages assigned to a student is minimized. If the allocation of books is not possible, return -1.
 */
+
+class Solution {
+public:
+    bool canAllocate(vector<int>& nums, int m, long long maxPages) {
+        int students = 1;
+        long long pages = 0;
+
+        for (int book : nums) {
+            if (pages + book <= maxPages) {
+                pages += book;
+            } else {
+                students++;
+                pages = book;
+
+                if (students > m)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    int findPages(vector<int>& nums, int m) {
+        int n = nums.size();
+
+        if (m > n)
+            return -1;
+
+        long long low = *max_element(nums.begin(), nums.end());
+        long long high = 0;
+
+        for (int x : nums)
+            high += x;
+
+        long long ans = -1;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (canAllocate(nums, m, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return (int)ans;
+    }
+};
