9282fb354aa055cb5255a0bd7f77ac6b750a8974
Naveen <rajnaveen13.av@gmail.com>
2026-06-22 21:12:53 +0530

    K-th Largest element in an array

--- SDE Sheet Top Coding Interview Problems/Day 22/K-thLargestelementinan_array.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 22/K-thLargestelementinan_array.cpp
@@ -4,3 +4,20 @@
    Input: nums = [1, 2, 3, 4, 5], k = 2
    Output: 4
 */
+
+class Solution {
+public:
+    int kthLargestElement(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int num : nums) {
+            pq.push(num);
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+};
