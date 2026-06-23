4cdd0e526696f9a780a2523f76b4f04e81e50fb1
Naveen <rajnaveen13.av@gmail.com>
2026-06-23 21:00:33 +0530

    Top K Frequent Elements

--- SDE Sheet Top Coding Interview Problems/Day 23/TopK _FrequentElements.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 23/TopK _FrequentElements.cpp
@@ -2,3 +2,26 @@
 /* Given an integer array nums and an integer k, return any order list of the k most frequent elements in nums.
    Your solution must run in better than O(n log n) time, where n = nums.length.
 */
+
+class Solution {
+public:
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        std::vector<std::pair<int, int>> bucket;
+        for (const auto& pair : freq) {
+            bucket.push_back(pair);
+        }
+        std::sort(bucket.begin(), bucket.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+            return a.second > b.second; 
+        });
+        std::vector<int> ans;
+        for (int i = 0; i < k; ++i) {
+            ans.push_back(bucket[i].first);
+        }
+
+        return ans;
+    }
+};
