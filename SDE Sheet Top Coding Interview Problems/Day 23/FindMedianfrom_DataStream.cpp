bd7cd67fbc07c3815dbdd81c1d48d2ed42320e5e
Naveen <rajnaveen13.av@gmail.com>
2026-06-23 20:54:46 +0530

    Find Median from Data Stream

--- SDE Sheet Top Coding Interview Problems/Day 23/FindMedianfrom_DataStream.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 23/FindMedianfrom_DataStream.cpp
@@ -8,3 +8,44 @@ void addNum(int num) adds the integer num to the data structure.
 double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
 
 */
+class MedianFinder {
+public:
+    priority_queue<int> LeftMaxHeap;
+    priority_queue<int, vector<int>, greater<int>> RightMinHeap;
+    MedianFinder() {}
+
+    void balanceHeap() {
+        if (RightMinHeap.size() > LeftMaxHeap.size()) {
+            LeftMaxHeap.push(RightMinHeap.top());
+            RightMinHeap.pop();
+        } else {
+            if (RightMinHeap.size() < LeftMaxHeap.size() - 1) {
+                RightMinHeap.push(LeftMaxHeap.top());
+                LeftMaxHeap.pop();
+            }
+        }
+    }
+
+    void addNum(int num) {
+        if (LeftMaxHeap.empty()) {
+            LeftMaxHeap.push(num);
+            return;
+        } else {
+            if (num > LeftMaxHeap.top()) {
+                RightMinHeap.push(num);
+            } else {
+                LeftMaxHeap.push(num);
+            }
+            balanceHeap();
+        }
+    }
+
+    double findMedian() {
+        if (LeftMaxHeap.size() > RightMinHeap.size()) {
+            return LeftMaxHeap.top();
+        } else {
+            double ans = (LeftMaxHeap.top() + RightMinHeap.top()) / 2.0;
+            return ans;
+        }
+    }
+};
