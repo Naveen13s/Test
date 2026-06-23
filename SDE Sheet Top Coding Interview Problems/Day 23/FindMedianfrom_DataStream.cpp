f4bda74e956931cee0403d66eb0eb850310062e7
Naveen <rajnaveen13.av@gmail.com>
2026-06-23 20:54:15 +0530

    Find Median from Data Stream

---
+++ SDE Sheet Top Coding Interview Problems/Day 23/FindMedianfrom_DataStream.cpp
@@ -0,0 +1,10 @@
+//Find Median from Data Stream
+/*Implement a class that finds the median from a data stream. The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+Implement the MedianFInder class as follows:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+*/
