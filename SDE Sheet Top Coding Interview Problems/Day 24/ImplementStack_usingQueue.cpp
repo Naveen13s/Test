ab956e5e7bc5ec45633e0a219f210190b436bb12
Naveen <rajnaveen13.av@gmail.com>
2026-06-24 20:41:10 +0530

    Implement Stack using Queue

---
+++ SDE Sheet Top Coding Interview Problems/Day 24/ImplementStack_usingQueue.cpp
@@ -0,0 +1,8 @@
+//Implement Stack using Queue
+/* Implement a Last-In-First-Out (LIFO) stack using a single queue. The implemented stack should support the following operations: push, pop, top, and isEmpty.
+   Implement the QueueStack class:
+   void push(int x): Pushes element x onto the stack.
+   int pop(): Removes and returns the top element of the stack.
+   int top(): Returns the top element of the stack without removing it.
+   boolean isEmpty(): Returns true if the stack is empty, false otherwise.
+*/
