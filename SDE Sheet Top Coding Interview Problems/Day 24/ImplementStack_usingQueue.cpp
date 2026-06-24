52d3f5071647bafc9e85044ffc7ccbcccc1e944b
Naveen <rajnaveen13.av@gmail.com>
2026-06-24 20:41:31 +0530

    Implement Stack using Queue

--- SDE Sheet Top Coding Interview Problems/Day 24/ImplementStack_usingQueue.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 24/ImplementStack_usingQueue.cpp
@@ -6,3 +6,37 @@
    int top(): Returns the top element of the stack without removing it.
    boolean isEmpty(): Returns true if the stack is empty, false otherwise.
 */
+
+class QueueStack {
+    queue<int> q;
+
+public:
+    void push(int x) {
+        q.push(x);
+
+        int sz = q.size();
+
+        for (int i = 0; i < sz - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        if (q.empty()) return -1;
+
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int top() {
+        if (q.empty()) return -1;
+
+        return q.front();
+    }
+
+    bool isEmpty() {
+        return q.empty();
+    }
+};
