49b38d074b373eeb47cccce24a07d5af1748723c
Naveen <rajnaveen13.av@gmail.com>
2026-06-25 20:26:06 +0530

    Balanced Paranthesis

--- SDE Sheet Top Coding Interview Problems/Day 25/Balanced_Paranthesis.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 25/Balanced_Paranthesis.cpp
@@ -1,3 +1,24 @@
 //Balanced Paranthesis
 /* Given string str containing just the characters '(', ')', '{', '}', '[' and ']', check if the input string is valid and return true if the string is balanced otherwise return false.*/
 
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+                st.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
