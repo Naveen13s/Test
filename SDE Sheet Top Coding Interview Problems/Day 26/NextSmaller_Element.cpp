75365b0fd4b9741e913dec4ae0a0f53fa98eb776
Naveen <rajnaveen13.av@gmail.com>
2026-06-26 21:35:05 +0530

    Next Smaller Element

--- SDE Sheet Top Coding Interview Problems/Day 26/NextSmaller_Element.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 26/NextSmaller_Element.cpp
@@ -3,3 +3,27 @@
    The Next Smaller Element for an element x is defined as the first element to the right of x that is smaller than x.
    If there is no smaller element to the right, then the NSE is -1.
 */
+class Solution {
+public:
+    vector<int> nextSmallerElements(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() >= arr[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                ans[i] = st.top();
+            }
+
+            st.push(arr[i]);
+        }
+
+        return ans;
+    }
+};
