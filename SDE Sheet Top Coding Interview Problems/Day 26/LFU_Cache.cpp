b8253c5abdbe991e1b573550b3ad7b7b5baa6629
Naveen <rajnaveen13.av@gmail.com>
2026-06-26 21:40:06 +0530

    LFU Cache

--- SDE Sheet Top Coding Interview Problems/Day 26/LFU_Cache.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 26/LFU_Cache.cpp
@@ -8,3 +8,64 @@
    When a key is first inserted into the cache, its use counter is set to 1 due to the put operation. The use counter for a key in the cache is incremented whenever a get or put operation is called on it.
    Ensure that the functions get and put run in O(1) average time complexity.
 */
+
+struct Node {
+  int key;
+  int value;
+  int freq;
+  list<int>::const_iterator it;
+};
+
+class LFUCache {
+ public:
+  LFUCache(int capacity) : capacity(capacity), minFreq(0) {}
+
+  int get(int key) {
+    if (!keyToNode.count(key))
+      return -1;
+
+    Node& node = keyToNode[key];
+    touch(node);
+    return node.value;
+  }
+
+  void put(int key, int value) {
+    if (capacity == 0)
+      return;
+    if (keyToNode.count(key)) {
+      Node& node = keyToNode[key];
+      node.value = value;
+      touch(node);
+      return;
+    }
+
+    if (keyToNode.size() == capacity) {
+      const int keyToEvict = freqToList[minFreq].back();
+      freqToList[minFreq].pop_back();
+      keyToNode.erase(keyToEvict);
+    }
+
+    minFreq = 1;
+    freqToList[1].push_front(key);
+    keyToNode[key] = {key, value, 1, cbegin(freqToList[1])};
+  }
+
+ private:
+  int capacity;
+  int minFreq;
+  unordered_map<int, Node> keyToNode;
+  unordered_map<int, list<int>> freqToList;
+
+  void touch(Node& node) {
+    const int prevFreq = node.freq;
+    const int newFreq = ++node.freq;
+    freqToList[prevFreq].erase(node.it);
+    if (freqToList[prevFreq].empty()) {
+      freqToList.erase(prevFreq);
+      if (prevFreq == minFreq)
+        ++minFreq;
+    }
+    freqToList[newFreq].push_front(node.key);
+    node.it = cbegin(freqToList[newFreq]);
+  }
+};
