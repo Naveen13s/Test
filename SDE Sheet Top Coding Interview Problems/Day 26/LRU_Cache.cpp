a5e1b19df1b3d1a2c5b4d4414b977f8e275d0422
Naveen <rajnaveen13.av@gmail.com>
2026-06-26 21:37:56 +0530

    LRU Cache

--- SDE Sheet Top Coding Interview Problems/Day 26/LRU_Cache.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 26/LRU_Cache.cpp
@@ -7,3 +7,43 @@
    The functions get and put must each run in O(1) average time complexity.
    Note : In Input is provided in 2D array format where the first number in each array denotes the operation (1-put, 2-get) to perform. The next integers are the values used for the operation.
 */
+
+class LRUCache {
+    int cap;
+
+    list<pair<int, int>> dll;   
+    unordered_map<int, list<pair<int, int>>::iterator> mp;
+
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+
+    int get(int key) {
+        if (mp.find(key) == mp.end())
+            return -1;
+
+        auto it = mp[key];
+        int value = it->second;
+        dll.erase(it);
+        dll.push_front({key, value});
+        mp[key] = dll.begin();
+
+        return value;
+    }
+
+    void put(int key, int value) {
+
+        if (mp.find(key) != mp.end()) {
+            dll.erase(mp[key]);
+        }
+        else if (dll.size() == cap) {
+            auto last = dll.back();
+            mp.erase(last.first);
+            dll.pop_back();
+        }
+
+        dll.push_front({key, value});
+        mp[key] = dll.begin();
+    }
+};
