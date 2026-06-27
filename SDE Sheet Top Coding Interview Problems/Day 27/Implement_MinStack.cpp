de984cd1ed9a2018fb4261098d6875f70060929d
Naveen <rajnaveen13.av@gmail.com>
2026-06-27 21:58:48 +0530

    Implement Min Stack

--- SDE Sheet Top Coding Interview Problems/Day 27/Implement_MinStack.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 27/Implement_MinStack.cpp
@@ -1,2 +1,32 @@
 //Implement Min Stack
 /* Design a stack that supports the following operations in constant time: push, pop, top, and retrieving the minimum element.*/
+
+class MinStack {
+private:
+    vector<vector<int>> st;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int min_val = getMin();
+        if (st.empty() || min_val > val) {
+            min_val = val;
+        }
+        st.push_back({val, min_val});        
+    }
+    
+    void pop() {
+        st.pop_back();
+    }
+    
+    int top() {
+        return st.empty() ? -1 : st.back()[0];
+    }
+    
+    int getMin() {
+        return st.empty() ? -1 : st.back()[1]; 
+    }
+};
