2b910f2da4da03000be8628a53cebfd925fd81a6
Naveen <rajnaveen13.av@gmail.com>
2026-06-27 21:56:51 +0530

    Largest rectangle in a histogram

--- SDE Sheet Top Coding Interview Problems/Day 27/Largestrectangleina_histogram.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 27/Largestrectangleina_histogram.cpp
@@ -1,3 +1,30 @@
 //Largest rectangle in a histogram
 /* Given an array of integers heights representing the histogram's bar height where the width of each bar is 1 return the area of the largest rectangle in the histogram.*/
 
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            maxArea = max(maxArea, heights[i] * width);
+        }
+        return maxArea;
+    }
+};
