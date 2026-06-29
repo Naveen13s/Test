75199c72cf490b78cb1523eb33a6b3ad9dc0e833
Naveen <rajnaveen13.av@gmail.com>
2026-06-29 22:26:29 +0530

    Reverse every word in a string

---
+++ SDE Sheet Top Coding Interview Problems/Day 29/Reverseeverywordina_string.cpp
@@ -0,0 +1,28 @@
+//Reverse every word in a string
+/* Given an input string, containing upper-case and lower-case letters, digits, and spaces( ' ' ). A word is defined as a sequence of non-space characters. The words in s are separated by at least one space.*/
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        int left = 0;
+        int right = 0;
+        int i = 0;
+        while (i < n) {
+            while (i < n && s[i] == ' ')
+                i++;
+            if (i == n)
+                break;
+            while (i < n && s[i] != ' ') {
+                s[right++] = s[i++];
+            }
+            reverse(s.begin() + left, s.begin() + right);
+            s[right++] = ' ';
+            left = right;
+            i++;
+        }
+        s.resize(right - 1);
+        return s;
+    }
+};
