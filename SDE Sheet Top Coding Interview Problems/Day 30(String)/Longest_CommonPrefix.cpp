b35b9883322a6f4e359582cb3a6b366b4cfb9135
Naveen <rajnaveen13.av@gmail.com>
2026-06-30 20:27:43 +0530

    Longest Common Prefix

--- SDE Sheet Top Coding Interview Problems/Day 30(String)/Longest_CommonPrefix.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 30(String)/Longest_CommonPrefix.cpp
@@ -2,3 +2,25 @@
 /* Write a function to find the longest common prefix string amongst an array of strings.
    If there is no common prefix, return an empty string "".
 */
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string lp = strs[0]; 
+
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0; 
+            while (j < lp.size() && j < strs[i].size()) {
+                if (lp[j] != strs[i][j]) {
+                    break;
+                }
+                j++;
+            }
+            lp.erase(j, string::npos);
+            if (lp == "") {
+                return lp;
+            }
+        }
+
+        return lp;
+    }
+};
