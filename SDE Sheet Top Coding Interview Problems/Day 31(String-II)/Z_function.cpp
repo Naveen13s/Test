037501ff6d0e26f42746a3ac4ac7eb2fdbf485e1
Naveen <rajnaveen13.av@gmail.com>
2026-07-01 22:27:04 +0530

    Z function

---
+++ SDE Sheet Top Coding Interview Problems/Day 31(String-II)/Z_function.cpp
@@ -0,0 +1,2 @@
+//Z function
+/* Given two strings, one is a text string, text, and the other is a pattern string, pattern. Print the indexes of all occurrences of the pattern string in the text string using the Z function algorithm. Use 0-based indexing while returning the indices.*/
