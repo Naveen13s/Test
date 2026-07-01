d53483e06ab93dc2212c2f04728455049761c52d
Naveen <rajnaveen13.av@gmail.com>
2026-07-01 22:31:49 +0530

    KMP Algorithm or LPS array

---
+++ SDE Sheet Top Coding Interview Problems/Day 31(String-II)/KMPAlgorithmor_LPSarray.cpp
@@ -0,0 +1,4 @@
+//KMP Algorithm or LPS array
+/* Given two strings, one is a text string, text, and the other is a pattern string, pattern. Find and print the indices of all the occurrences of the pattern string within the text string. Use 0-based indexing for the indices, and ensure that the indices are in ascending order. If the pattern does not occur in the text, return an empty list.
+   Implement this solution using the Knuth-Morris-Pratt (KMP) algorithm for efficient pattern matching.
+*/
