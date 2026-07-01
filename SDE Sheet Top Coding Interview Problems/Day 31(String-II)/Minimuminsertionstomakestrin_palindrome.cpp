6c4fe5bc60dcee1f62a01aa5e3b6df164c8c0168
Naveen <rajnaveen13.av@gmail.com>
2026-07-01 22:30:14 +0530

    Minimum insertions to make string palindrome

--- SDE Sheet Top Coding Interview Problems/Day 31(String-II)/Minimuminsertionstomakestrin_palindrome.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 31(String-II)/Minimuminsertionstomakestrin_palindrome.cpp
@@ -1,2 +1,25 @@
 //Minimum insertions to make string palindrome
 /* Given a string s, find the minimum number of insertions needed to make it a palindrome. A palindrome is a sequence that reads the same backward as forward. You can insert characters at any position in the string.*/
+
+class Solution {
+   public:
+    int minInsertion(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        int n = s.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == rev[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return n - dp[n][n];
+    }
+};
