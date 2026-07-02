afc2c03b82b323badfc73a995808ebdc4a482e05
Naveen <rajnaveen13.av@gmail.com>
2026-07-02 20:39:51 +0530

    Valid Anagram

--- SDE Sheet Top Coding Interview Problems/Day 32 (String)/Valid_Anagram.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 32 (String)/Valid_Anagram.cpp
@@ -1,2 +1,21 @@
 //Valid Anagram
 /* Given two strings s and t, return true if t is an anagram of s, and false otherwise. */
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        int arr[26] = {0};
+
+        for (int i = 0 ; i<s.length(); i++){
+            arr[s[i]-'a'] += 1;
+            arr[t[i] - 'a'] -= 1;
+        }
+
+        for (int check : arr){
+            if (check != 0) return false;
+        }
+        return true;
+    }
+};
