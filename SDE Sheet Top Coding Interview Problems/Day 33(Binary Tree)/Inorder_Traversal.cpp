f86de8c808904abcda5a5a3215b48fc3bc72e3ec
Naveen <rajnaveen13.av@gmail.com>
2026-07-03 20:49:00 +0530

    Inorder Traversal

---
+++ SDE Sheet Top Coding Interview Problems/Day 33(Binary Tree)/Inorder_Traversal.cpp
@@ -0,0 +1,31 @@
+//Inorder Traversal
+/* Given root of binary tree, return the Inorder traversal of the binary tree.*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    vector<int> ans;
+
+    void dfs(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        dfs(root->left);
+        ans.push_back(root->data);   
+        dfs(root->right);
+    }
+
+    vector<int> inorder(TreeNode* root) {
+        ans.clear();
+        dfs(root);
+        return ans;
+    }
+};
