13a408e6f8610282e258083a09812dacec00ba9a
Naveen <rajnaveen13.av@gmail.com>
2026-07-04 20:43:42 +0530

    Top View of BT

---
+++ SDE Sheet Top Coding Interview Problems/Day 34(BT)/Top_ViewofBT.cpp
@@ -0,0 +1,6 @@
+//Top View of BT
+/* Given the root of a binary tree, return the top view of the binary tree.
+   The top view of a binary tree consists of the set of nodes visible when the tree is observed from above.
+   Return the values of these nodes ordered from the leftmost to the rightmost position.
+   If multiple nodes share the same horizontal distance from the root, only the node that appears first when traversing from left to right (i.e., the leftmost node) should be included in the result.
+*/
