adfa081a68f7643913974e8da8df40a2bc1ce60b
Naveen <rajnaveen13.av@gmail.com>
2026-07-04 20:41:32 +0530

    Bottom view of BT

---
+++ SDE Sheet Top Coding Interview Problems/Day 34(BT)/Bottom_viewofBT.cpp
@@ -0,0 +1,4 @@
+//Bottom view of BT
+/* Given root of binary tree, return the bottom view of the binary tree.
+   The bottom view of a binary tree is the set of nodes visible when the tree is viewed from the bottom. Return nodes from the leftmost node to the rightmost node. Also if 2 nodes are outside the shadow of the tree and are at the same position then consider the node that appears later in level traversal.
+*/
