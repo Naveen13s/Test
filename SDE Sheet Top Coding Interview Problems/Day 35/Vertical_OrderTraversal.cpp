332112a06a8b12a09764a38e81abf66125d352f2
Naveen <rajnaveen13.av@gmail.com>
2026-07-05 12:16:08 +0530

    ertical Order Traversal

---
+++ SDE Sheet Top Coding Interview Problems/Day 35/Vertical_OrderTraversal.cpp
@@ -0,0 +1,5 @@
+//Vertical Order Traversal
+/* Compute the binary tree's vertical order traversal given its root.
+   The left and right children of a node at location (row, col) will be at (row + 1, col - 1) and (row + 1, col + 1), respectively. The tree's root is located at (0, 0).
+   The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting from the leftmost column and ending on the rightmost column. There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values. Return the binary tree's vertical order traversal.
+*/
