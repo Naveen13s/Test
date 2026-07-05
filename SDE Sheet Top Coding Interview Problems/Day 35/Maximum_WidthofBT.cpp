ca47c7d20a27924d19462c35bb902f896f7ef642
Naveen <rajnaveen13.av@gmail.com>
2026-07-05 12:19:49 +0530

    Maximum Width of BT

---
+++ SDE Sheet Top Coding Interview Problems/Day 35/Maximum_WidthofBT.cpp
@@ -0,0 +1,4 @@
+//Maximum Width of BT
+/* Given the root of a binary tree, return the maximum width of the given tree.
+   The maximum width of a tree is the maximum width among all levels. The width of a level is determined by measuring the distance between its end nodes, which are the leftmost and rightmost non-null nodes. The length calculation additionally takes into account the null nodes that would be present between the end nodes if a complete binary tree were to stretch down to that level.
+*/
