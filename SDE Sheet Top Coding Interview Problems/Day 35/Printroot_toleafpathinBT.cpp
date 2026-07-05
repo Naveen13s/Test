6f0edebbd9a3fa4595f5e12c69bef15a75f3bfac
Naveen <rajnaveen13.av@gmail.com>
2026-07-05 12:18:32 +0530

    Print root to leaf path in BT

--- SDE Sheet Top Coding Interview Problems/Day 35/Printroot_toleafpathinBT.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 35/Printroot_toleafpathinBT.cpp
@@ -2,3 +2,29 @@
 /* Given the root of a binary tree. Return all the root-to-leaf paths in the binary tree.
    A leaf node of a binary tree is the node which does not have a left and right child.
 */
+
+
+class Solution {
+   public:
+    void dfs(TreeNode* root, vector<int>& path, vector<vector<int>>& ans) {
+        if (root == NULL) return;
+        path.push_back(root->data);
+        if (root->left == NULL && root->right == NULL) {
+            ans.push_back(path);
+        } else {
+            dfs(root->left, path, ans);
+            dfs(root->right, path, ans);
+        }
+
+        path.pop_back();  // Backtrack
+    }
+
+    vector<vector<int>> allRootToLeaf(TreeNode* root) {
+        vector<vector<int>> ans;
+        vector<int> path;
+
+        dfs(root, path, ans);
+
+        return ans;
+    }
+};
