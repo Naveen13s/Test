1c1c45c34aea40fdaeb83d254cbd1ba709664e21
Naveen <rajnaveen13.av@gmail.com>
2026-07-05 12:18:04 +0530

    Print root to leaf path in BT

---
+++ SDE Sheet Top Coding Interview Problems/Day 35/Printroot_toleafpathinBT.cpp
@@ -0,0 +1,4 @@
+//Print root to leaf path in BT
+/* Given the root of a binary tree. Return all the root-to-leaf paths in the binary tree.
+   A leaf node of a binary tree is the node which does not have a left and right child.
+*/
