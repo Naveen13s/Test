f8a80e57de07b80f64ec539cb2978c87dabcabf8
Naveen <rajnaveen13.av@gmail.com>
2026-07-07 14:39:20 +0530

    Boundary Traversal

--- SDE Sheet Top Coding Interview Problems/Day 37(BT-II)/Boundary_Traversal.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 37(BT-II)/Boundary_Traversal.cpp
@@ -9,3 +9,62 @@
     - The leftmost leaf is not in the left boundary.
    The right boundary is similar to the left boundary, except it is the right side of the root's right subtree. Again, the leaf is not part of the right boundary, and the right boundary is empty if the root does not have a right child.
 */
+
+class Solution {
+   public:
+    bool isLeaf(TreeNode* node) {
+        return node->left == NULL && node->right == NULL;
+    }
+    void addLeftBoundary(TreeNode* root, vector<int>& ans) {
+        TreeNode* curr = root->left;
+        while (curr) {
+            if (!isLeaf(curr)) ans.push_back(curr->data);
+            if (curr->left)
+                curr = curr->left;
+            else
+                curr = curr->right;
+        }
+    }
+
+    void addLeaves(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) return;
+
+        if (isLeaf(root)) {
+            ans.push_back(root->data);
+            return;
+        }
+
+        addLeaves(root->left, ans);
+        addLeaves(root->right, ans);
+    }
+
+    void addRightBoundary(TreeNode* root, vector<int>& ans) {
+        TreeNode* curr = root->right;
+        vector<int> temp;
+
+        while (curr) {
+            if (!isLeaf(curr)) temp.push_back(curr->data);
+
+            if (curr->right)
+                curr = curr->right;
+            else
+                curr = curr->left;
+        }
+
+        for (int i = temp.size() - 1; i >= 0; i--) ans.push_back(temp[i]);
+    }
+
+    vector<int> boundary(TreeNode* root) {
+        vector<int> ans;
+
+        if (root == NULL) return ans;
+
+        if (!isLeaf(root)) ans.push_back(root->data);
+
+        addLeftBoundary(root, ans);
+        addLeaves(root, ans);
+        addRightBoundary(root, ans);
+
+        return ans;
+    }
+};
