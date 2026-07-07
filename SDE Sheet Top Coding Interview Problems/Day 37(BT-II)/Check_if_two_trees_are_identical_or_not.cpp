6ef7511f0c40240eb8d7415124eb2eae7c7ced48
Naveen <rajnaveen13.av@gmail.com>
2026-07-07 14:33:29 +0530

    Check if two trees are identical or not

---
+++ SDE Sheet Top Coding Interview Problems/Day 37(BT-II)/Check_if_two_trees_are_identical_or_not.cpp
@@ -0,0 +1,4 @@
+//Check if two trees are identical or not
+/* Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+   Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+*/
