b865ca7cd3baba5e1ed60378e842e294726fa667
Naveen <rajnaveen13.av@gmail.com>
2026-07-07 14:31:48 +0530

    LCA in BT

--- SDE Sheet Top Coding Interview Problems/Day 37(BT-II)/LCA_in_BT.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 37(BT-II)/LCA_in_BT.cpp
@@ -3,3 +3,24 @@
    The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).
    Note: Return the TreeNode itself, not its value
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int data;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+ * };
+ **/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        if (root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        return left ? left : right;
+    }
+};
