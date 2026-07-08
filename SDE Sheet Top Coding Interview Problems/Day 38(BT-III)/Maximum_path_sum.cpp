ac139007849eb4e8c73d95835951077b112f537e
Naveen <rajnaveen13.av@gmail.com>
2026-07-08 18:33:19 +0530

    Maximum path sum

--- SDE Sheet Top Coding Interview Problems/Day 38(BT-III)/Maximum_path_sum.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 38(BT-III)/Maximum_path_sum.cpp
@@ -11,3 +11,31 @@
    - Update the global max (ans) with the path going through this node: node.val + leftGain + rightGain.
    - Return to the parent: node.val + max(leftGain, rightGain) since parent can only continue on one side.
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = INT_MIN;
+
+    int helper(TreeNode* node) {
+        if (!node) return 0;
+        int left = max(helper(node->left), 0);
+        int right = max(helper(node->right), 0);
+        ans = max(ans, node->val + left + right);
+        return node->val + max(left, right);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        helper(root);
+        return ans;
+    }
+};
