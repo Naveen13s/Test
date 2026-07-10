d476f7a8879ff8bb14d9675b8d84d4e319b42911
Naveen <rajnaveen13.av@gmail.com>
2026-07-10 22:41:31 +0530

    Convert Sorted Array to Binary Search Tree

---
+++ SDE Sheet Top Coding Interview Problems/Day 40(BST)/Convert Sorted_Array_to_Binary_Search_Tree.cpp
@@ -0,0 +1,27 @@
+//Convert Sorted Array to Binary Search Tree
+/* Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums, 0, nums.size());
+    }
+    TreeNode* build(vector<int>& nums, int start, int end) {
+        if (start == end) return nullptr;
+        int mid = (start + end) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, start, mid);
+        root->right = build(nums, mid + 1, end);
+        return root;
+    }
+};
--- SDE Sheet Top Coding Interview Problems/Day 40(BST)/Search_in_BST.cpp
+++ SDE Sheet Top Coding Interview Problems/Day 40(BST)/Search_in_BST.cpp
@@ -2,3 +2,32 @@
 /* Given the root of a binary search tree (BST) and an integer val.
    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root, int val) {
+        if (root == NULL)
+            return NULL;
+
+        if (root->val == val)
+            return root;
+
+        if (root->val < val)
+            return solve(root->right, val);
+        else
+            return solve(root->left, val);
+    }
+
+    TreeNode* searchBST(TreeNode* root, int val) { return solve(root, val); }
+};
